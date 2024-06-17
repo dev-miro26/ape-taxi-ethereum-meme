00127267284eceae21e9f2de61fb3e1d19f9241f
dev-miro26 <gabrielzhang833@gmail.com>
2024-06-17 00:59:06 -0700

    add audio

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Home = () => {
+const Home = ({ playPause }) => {
   return (
     <div className="home">
       <div className="home__gif">
         <img src="assets/images/3.gif" alt="3" />
       </div>
-      <div className="home__sound">
+      <div className="home__sound" onClick={playPause}>
         <img src="assets/images/moff.png" alt="moff" />
       </div>
     </div>
